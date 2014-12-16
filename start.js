63139a07c8bb559b32becb71d9e2833eb65276e3
Kevin Altman <kevin@itsthatguy.com>
2014-12-16 12:21:04 -0500

    gulp should be run when starting the server

--- start.js
+++ start.js
@@ -2,11 +2,13 @@
 var runCommand = require('run-command'),
     dotenv     = require('dotenv');
 
-dotenv.load()
+dotenv.load();
 
 console.log("[NODE_ENV] " + process.env.NODE_ENV);
 
 runCommand("coffee", ['app.coffee']);
 if (process.env.NODE_ENV == "development") {
-  runCommand("gulp", ['watch']);
+  runCommand("gulp", ['default'], function() {
+    runCommand("gulp", ['watch']);
+  });
 }
